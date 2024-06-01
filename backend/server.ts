d185032d9e0d66c5c77098858b101912d4c2617f
Hossein <rezaeig22@gmail.com>
2024-06-01 21:23:06 +0330

    Express Server & Initial Product Routes

---
+++ backend/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import products from "./data/products";
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("API is running...");
+});
+
+app.get('/api/products', (req, res) => {
+    res.send(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.send(product);
+});
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
