44c72eac2fc464019b95cb42c34d9b773550aa6f
Hossein <rezaeig22@gmail.com>
2024-06-01 13:56:50 +0330

    Rating Component

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,5 +1,6 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 export interface Product {
   _id: string;
@@ -26,10 +27,13 @@ const Product = ({ product }: Props) => {
       </Link>
       <Card.Body>
         <Link to={`product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as="div">
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </Card.Text>
         <Card.Text as="h3">{product.price}</Card.Text>
       </Card.Body>
     </Card>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 // import "./assets/styles/index.css";
 import App from "./App.tsx";
 import HomeScreen from "./screens/HomeScreen.tsx";
