b951f596ce8221c4e1e094bb5d2d24d9a07e0e41
Hossein <rezaeig22@gmail.com>
2024-06-02 13:01:39 +0330

    Product Details Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,17 +6,18 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
-// import "./assets/styles/index.css";
 import App from "./App.tsx";
 import HomeScreen from "./screens/HomeScreen.tsx";
+import ProductScreen from "./screens/ProductScreen.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
