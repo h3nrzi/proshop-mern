15ce7f2e7806f3d1f7016cb6a3c5d3a8e8271c7d
Hossein <rezaeig22@gmail.com>
2024-06-04 20:31:01 +0330

    Cart Slice & Reducer

--- frontend/src/app/reducer.ts
+++ frontend/src/app/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import api from "../api/api";
+import cart from "./cart";
 
 const reducer = combineReducers({
   [api.reducerPath]: api.reducer,
+  [cart.name]: cart.reducer,
 });
 
 export default reducer;
