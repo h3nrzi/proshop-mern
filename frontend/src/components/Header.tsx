cecb0e0e55f51aa84cd355258844f40a4e4b8f14
Hossein <rezaeig22@gmail.com>
2024-06-05 19:51:33 +0330

    Item Count In Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,13 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store";
 
 const Header = () => {
+  const { cartItems } = useSelector((rootState: RootState) => rootState.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -20,9 +24,15 @@ const Header = () => {
               <Link to="/cart" className="text-decoration-none">
                 <Nav.Link as="span">
                   <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge bg="info" className="ms-2">
+                      {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </Link>
-              <Link to="/login" className="text-decoration-none">
+
+              <Link to="/login" className="text-decoration-none ms-3">
                 <Nav.Link as="span">
                   <FaUser /> Sign In
                 </Nav.Link>
